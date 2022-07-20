b8ee5404283810fd0730fbb14780f41c57f5eb5e
mahimahi47 <109627130+mahimahi47@users.noreply.github.com>
2022-07-20 19:34:41 +0530

    Update script.js

--- script.js
+++ script.js
@@ -168,7 +168,7 @@ function checkImageWithNyckel(image)
     formdata.append('file', image);
 
     $.ajax({
-        url:'https://www.nyckel.com/v1/functions/pxvrvn451z7vtd44/invoke',
+        url:'https://www.nyckel.com/v1/functions/erm8aaifejk5faf7/invoke',
         type: 'post',
         data: formdata,
         contentType: false,
